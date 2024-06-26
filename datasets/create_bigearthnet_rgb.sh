f33282f8aa121a4cff312c438d34d02a052ed05d
rhprasad0 <rhprasad@outlook.com>
2024-06-26 09:28:20 -0400

    Added frootprints, removed intermerdiate tif

--- datasets/create_bigearthnet_rgb.sh
+++ datasets/create_bigearthnet_rgb.sh
@@ -1,27 +1,37 @@
 #! /bin/bash
 # You will need to install GNU Parallel to use this script!
-# My CPU has 16 cores, and I hear n+1 is the sweet spot
 
-rm -r ./output/bigearthnetcog
-mkdir ./output/bigearthnetcog
+export bigearthnet_path=./data/BigEarthNet
+export output_path=./output/bigearthnetrgb
+
+mkdir $output_path
+mkdir $output_path/raster
+mkdir $output_path/vector
 
 function create_rgb_composite() {    
-    gdalbuildvrt -separate ./data/BigEarthNet/BigEarthNet-v1.0/$1/stack.vrt \
-        ./data/BigEarthNet/BigEarthNet-v1.0/$1/${1}_B04.tif \
-        ./data/BigEarthNet/BigEarthNet-v1.0/$1/${1}_B03.tif \
-        ./data/BigEarthNet/BigEarthNet-v1.0/$1/${1}_B02.tif
+    gdalbuildvrt -separate $bigearthnet_path/BigEarthNet-v1.0/$1/stack.vrt \
+        $bigearthnet_path/BigEarthNet-v1.0/$1/${1}_B04.tif \
+        $bigearthnet_path/BigEarthNet-v1.0/$1/${1}_B03.tif \
+        $bigearthnet_path/BigEarthNet-v1.0/$1/${1}_B02.tif
 
     gdal_translate -of COG \
-        ./data/BigEarthNet/BigEarthNet-v1.0/$1/stack.vrt \
-        ./output/bigearthnetcog/${1}.tif
+        $bigearthnet_path/BigEarthNet-v1.0/$1/stack.vrt \
+        $output_path/raster/${1}.tif
 
     gdalwarp -t_srs EPSG:4326 \
-        "./output/bigearthnetcog/${1}.tif" \
-        "./output/bigearthnetcog/${1}_RGB.tif"
+        $output_path/raster/${1}.tif \
+        $output_path/raster/${1}_RGB.tif
+
+    gdal_footprint $output_path/raster/${1}_RGB.tif $output_path/vector/${1}.geojson
+
+    rm $output_path/raster/${1}.tif
+
 }
 export -f create_rgb_composite
 
 while IFS="," read -r s2name s1name
 do
-    parallel -j 17 create_rgb_composite ::: $s2name
+    # The -j flag is the number of cores you want to use
+    parallel -j 12 create_rgb_composite ::: $s2name
+
 done < <(cut -d "," -f1 ./data/BigEarthNet/bigearthnet-test.csv)
